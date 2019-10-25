75b7c0d227826fa7ce2477b6ded0d6c33c4b4281
Влад Мотрунич <onistarvlaad@gmail.com>
2019-10-25 14:59:41 +0300

    start

---
+++ Assets/Resources/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int health = 3;
+    public float time = 5;
+    private void OnMouseDown()
+    {
+        health--;
+        if (health < 1)
+        {
+            Destroy(gameObject);
+
+            var score = GameObject.Find("Score").GetComponent<Text>();
+            score.text = (Convert.ToInt64(score.text) + 1).ToString();
+        }
+
+    }
+
+    private void Start()
+    {
+        time = Time.time;
+    }
+    private void Update()
+    {
+        var heroHealth = GameObject.FindGameObjectsWithTag("health");
+        if (Time.time - time >= 5)
+        {
+            if (heroHealth.Length > 0) Destroy(heroHealth[0]);
+            time = Time.time;
+        }
+    }
+
+}
---
+++ Assets/Resources/Scripts/Spawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Spawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float time;
+    Canvas canvas;
+    void Start()
+    {
+        time = Time.time;
+    }
+    private bool spawn(int position)
+    {
+        if (GameObject.Find("Enemy " + position) == null &&
+           GameObject.Find("Enemy " + position + "(Clone)") == null)
+        {
+            Instantiate(Resources.Load("Enemies/Enemy " + position), this.transform);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - time >= 5)
+        {
+            if(spawn(Convert.ToInt32(UnityEngine.Random.Range(1, 7)))) time = Time.time;
+        }
+        
+    }
+}
