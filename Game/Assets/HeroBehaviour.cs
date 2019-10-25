8088dc2a65520d8065576f879ec6deef27a86f41
Влад Мотрунич <onistarvlaad@gmail.com>
2019-10-25 18:08:10 +0300

    fixes

---
+++ Game/Assets/HeroBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroBehaviour : MonoBehaviour
+{
+    public int Health { get; private set; }
+    void Start()
+    {
+        Health = 3;
+        GameObject.Find("health");
+        for (int i = 0; i < Health; i++)
+        {
+            Instantiate(Resources.Load("Components/heart"),);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
